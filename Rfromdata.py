a22f0e859c0af1f2f9b3070e4eebda5de93e0151
Brooke Istvan <brookeistvan@college.harvard.edu>
2019-02-22 02:19:48 -0500

    from langstons

--- Rfromdata.py
+++ Rfromdata.py
@@ -20,13 +20,26 @@ tweet_files = sorted(os.listdir(tweet_files_dir))
 # Prepend directory to each tweet filename
 tweet_files = [tweet_files_dir+'/'+filename for filename in tweet_files]
 
+def isrecovered(username):
+    for key, value in users_by_day.items():
+        if key > newdaycount:
+            if username in users_by_day[key]:
+                return False
+    return True
+
+
 # key would be date or day number and value is set us users appearing then
 users_by_day = {}
+user_num_by_day = []
+tweets_by_day = []
 
 # # Loop over all tweet files
 dates = []
+daycount = 0
 for tweet_file in tweet_files:
-    todaysusers = {"hello"}
+    day_tweets = []
+    daycount += 1
+    todaysusers = {"removeme"}
     dates.append(str((tweet_file.split(".")[0]).split("/")[1]))
     with open(tweet_file, 'r') as f:
         # Go through each tweet in the tweet file
@@ -42,7 +55,48 @@ for tweet_file in tweet_files:
                     user = tweet["actor"]["preferredUsername"]
                     if user not in todaysusers:
                         todaysusers.add(user)
-    print(todaysusers)
+        
+            day_tweets.append(tweet)
+        tweets_by_day.append(len(day_tweets))
+    todaysusers.remove("removeme")
+    users_by_day.update({daycount:todaysusers})
+    user_num_by_day.append(len(todaysusers))
+# print(users_by_day)
+
+recoverdcount_by_day= []
+newdaycount = 0
+for tweet_file in tweet_files:
+    newdaycount += 1
+    recoverdcount = 0
+
+    # dates.append(str((tweet_file.split(".")[0]).split("/")[1]))
+    with open(tweet_file, 'r') as f:
+        # Go through each tweet in the tweet file
+        for line in f:
+            try:
+                tweet = json.loads(line.strip("\n"))
+            except:
+                print("Load error")
+                continue
+
+            if "actor" in tweet:
+                if "preferredUsername" in tweet["actor"]:
+                    if isrecovered(tweet["actor"]["preferredUsername"]) is True:
+                        recoverdcount += 1
+
+        recoverdcount_by_day.append(recoverdcount)
+print(recoverdcount_by_day)
+print(user_num_by_day)
+print(tweets_by_day)
+
+recoverdcount_by_day.remove(0)
+user_num_by_day.remove(0)
+
+count = 0
+totalrecoved_by_day = []
+for r in recoveredcount_by_day: 
+    count += r 
+    totalrecoved_by_day.append(count)
 
 
 #         new_users_by_day.append(newusercount)
@@ -51,8 +105,10 @@ for tweet_file in tweet_files:
 # new_users_by_day.remove(0)
 # old_users_by_day.remove(0)
 
-# a = np.array(new_users_by_day, dtype=np.float)
-# b = np.array(old_users_by_day, dtype=np.float)
+a = np.array(recoverdcount_by_day, dtype=np.float)
+b = np.array(user_num_by_day, dtype=np.float)
+
+fractionrecovered = a/b
 
 # total_users_by_day = a + b 
 
@@ -60,15 +116,15 @@ for tweet_file in tweet_files:
 
 # fraction_new = a/c
 # fraction_old = b/c
-
-# dates.remove('')
-# x = [dt.datetime.strptime(d,'%Y-%m-%d').date() for d in dates]
-# plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b'))
-# plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
-# plt.plot(x, fraction_new, label="new users")
+# print(dates)
+dates.remove('')
+x = [dt.datetime.strptime(d,'%Y-%m-%d').date() for d in dates]
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+plt.plot(x, totalrecoved_by_day)
 # plt.plot(x, fraction_old, label="old users")
-# plt.xlabel("time")
-# plt.ylabel("fraction of users")
+plt.xlabel("time")
+plt.ylabel("total number of users recovered")
 # plt.legend(loc='upper left')
-# plt.gcf().autofmt_xdate()
-# plt.show()
+plt.gcf().autofmt_xdate()
+plt.show()
